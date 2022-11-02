b04c93a1bcf0bee9bc382fde020e50cb8dbb4a54
Arfan017 <59225236+Arfan017@users.noreply.github.com>
2022-11-02 09:05:29 +0900

    Project Game Settings

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Audio;
+using System;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject panelSetting;
+    [SerializeField] TMP_Dropdown dropdownValue;
+    [SerializeField] AudioSource audioSource;
+    public AudioMixer mixer;
+
+    private void Start()
     {
-        
+        audioSource.loop = true;
+        audioSource.playOnAwake = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
-        
+        Debug.Log("Game Belum Diset :)");
+    }
+
+    public void PanelSetting(bool value)
+    {
+        panelSetting.SetActive(value);
+    }
+
+    public void getResolution(int value)
+    {
+        var Res = dropdownValue.options[value].text;
+        Debug.Log("resolusi: " + Res);
+    }
+
+    public void SetMute(bool value)
+    {
+        audioSource.mute = value;
+        Debug.Log("Mute: " + value);
+    }
+
+    public void SetVol(float sliderValue)
+    {
+        var vol = Mathf.Log10(sliderValue) * 20;
+        mixer.SetFloat("MusicVol", vol);
+        Debug.Log(vol);
     }
 }
